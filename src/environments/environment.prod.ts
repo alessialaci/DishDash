552e687906188123f0d0ce9084639c35bd758ec2
Alessia Lacitignola <alelaci21@gmail.com>
2023-04-30 16:08:32 +0200

    Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAMLO5xAiElT_dALE01YFEtSE4NEttZShA",
+    authDomain: "dishdash-26e0b.firebaseapp.com",
+    databaseURL: "https://dishdash-26e0b-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "dishdash-26e0b",
+    storageBucket: "dishdash-26e0b.appspot.com",
+    messagingSenderId: "547101967860",
+    appId: "1:547101967860:web:ece5d7950bb0634c4ea5a1"
+  }
 };
